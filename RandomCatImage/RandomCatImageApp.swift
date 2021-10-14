5b64fabdc7a2c21b42e86b83a90dd4ccd2d59a63
Emily Liang <eliang@lcs.on.ca>
2021-10-14 08:53:32 -0400

    Initial Commit

---
+++ RandomCatImage/RandomCatImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCatImageApp.swift
+//  RandomCatImage
+//
+//  Created by Emily Liang on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct RandomCatImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
